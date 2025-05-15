0476b3b323de7802f3df0110cd87084b3d113873
keanknowsss <solomon.keanu.sk@gmail.com>
2025-05-15 08:36:05 +0800

    create layout for livewire pages

--- app/Livewire/Profile.php
+++ app/Livewire/Profile.php
@@ -2,15 +2,16 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('layouts.app_lw')]
+#[Title('Profile')]
 class Profile extends Component
 {
     public function render()
     {
-        return view('livewire.profile')
-            ->layout('layouts.app', [
-                'title' => 'Profile'
-            ]);
+        return view('livewire.profile');
     }
 }
---
+++ resources/views/layouts/app_lw.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="shortcut icon" href="{{ asset('assets/favicons/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/favicons/favicon-16x16.png') }}" sizes="16x16" type="image/png">
+    <link rel="icon" href="{{ asset('assets/favicons/favicon-32x32.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ asset('assets/favicons/android-chrome-192x192.png') }}" sizes="192x192" type="image/png">
+    <link rel="icon" href="{{ asset('assets/favicons/android-chrome-512x512.png') }}" sizes="512x512" type="image/png">
+
+    @livewireStyles
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @stack('styles')
+
+    <title>{{ $title ?? 'Joke! Have a laugh' }}</title>
+</head>
+
+<body>
+    {{ $slot }}
+
+    @livewireScripts
+
+    @stack('scripts')
+</body>
+
+</html>
